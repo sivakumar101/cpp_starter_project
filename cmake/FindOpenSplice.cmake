28751c181ece5eaf7caa5d54a3773fcd942d777c
sivakumar101 <sivakumar.s.k.k@gmail.com>
2021-10-13 16:01:55 +1030

    Add support for opensplice source compilation

---
+++ cmake/FindOpenSplice.cmake
@@ -0,0 +1,115 @@
+##############################################################################
+# Try to find OpenSplice
+# Once done this will define:
+#
+#  OpenSplice_FOUND - system has OpenSplice.
+#  OpenSplice_INCLUDE_DIRS - the OpenSplice include directory.
+#  OpenSplice_LIBRARIES - Link these to use OpenSplice.
+#  OpenSplice_IDLGEN_BINARY - Binary for the IDL compiler.
+#
+# You need the environment variable $OSPL_HOME to be set to your OpenSplice
+# installation directory.
+# This script also includes the MacroOpenSplice.cmake script, which is useful
+# for generating code from your idl.
+#
+##############################################################################
+# Courtesy of Ivan Galvez Junquera <ivgalvez@gmail.com>
+##############################################################################
+FIND_PATH(OpenSplice_INCLUDE_DIR
+NAMES
+ccpp_dds_dcps.h
+#dds_dcps.idl
+PATHS
+$ENV{OSPL_HOME}/install/HDE/$ENV{SPLICE_TARGET}/include/dcps/C++/SACPP
+#$ENV{OSPL_HOME}/etc/idl/
+)
+
+SET(OpenSplice_INCLUDE_DIRS
+	#${OpenSplice_INCLUDE_DIR}
+$ENV{OSPL_HOME}/install/HDE/$ENV{SPLICE_TARGET}/include
+$ENV{OSPL_HOME}/install/HDE/$ENV{SPLICE_TARGET}/include/sys
+$ENV{OSPL_HOME}/etc/idl/
+$ENV{OSPL_HOME}/include/dcps/C++/isocpp2
+)
+
+# Find libraries
+FIND_LIBRARY(DCPSGAPI_LIBRARY
+NAMES
+dcpsgapi
+PATHS
+$ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET}
+)
+
+FIND_LIBRARY(DCPSSACPP_LIBRARY
+NAMES
+dcpssacpp
+PATHS
+$ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET}
+)
+
+FIND_LIBRARY(DDSKERNEL_LIBRARY
+NAMES
+ddskernel
+PATHS
+$ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET}
+)
+
+
+
+FIND_LIBRARY(DCPSISOCPP_LIBRARY
+NAMES
+dcpsisocpp2
+PATHS
+$ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET}
+)
+
+message(STATUS $ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET})
+message(STATUS $ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET})
+message(STATUS $ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET})
+message(STATUS $ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET})
+message(STATUS $ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET})
+message(STATUS $ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET})
+message(STATUS $ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET})
+message(STATUS $ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET})
+
+FIND_LIBRARY(DDSDATABASE_LIBRARY
+NAMES
+ddsdatabase
+PATHS
+$ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET}
+)
+
+FIND_LIBRARY(DDSOS_LIBRARY
+NAMES
+ddsos
+PATHS
+$ENV{OSPL_HOME}/lib/$ENV{SPLICE_TARGET}
+)
+
+SET(OpenSplice_LIBRARIES
+${DCPSGAPI_LIBRARY}
+${DCPSSACPP_LIBRARY}
+${DDSDATABASE_LIBRARY}
+${DDSOS_LIBRARY}
+${DDSKERNEL_LIBRARY}
+${DCPSISOCPP_LIBRARY}
+)
+
+# Binary for the IDL compiler
+#SET (OpenSplice_IDLGEN_BINARY $ENV{OSPL_HOME}/exec/$ENV{SPLICE_TARGET}/idlpp -I $ENV{OSPL_HOME}/etc/idl/)
+SET (OpenSplice_IDLGEN_BINARY $ENV{OSPL_HOME}/bin/idlpp -I $ENV{OSPL_HOME}/etc/idlpp)
+
+IF (OpenSplice_INCLUDE_DIRS AND OpenSplice_LIBRARIES)
+SET(OpenSplice_FOUND TRUE)
+ENDIF (OpenSplice_INCLUDE_DIRS AND OpenSplice_LIBRARIES)
+
+IF (OpenSplice_FOUND)
+MESSAGE(STATUS "Found Vortex OpenSplice libraries: ${OpenSplice_LIBRARIES}")
+ELSE (OpenSplice_FOUND)
+IF (OpenSplice_FIND_REQUIRED)
+MESSAGE(FATAL_ERROR "Could not find Vortex OpenSplice")
+ENDIF (OpenSplice_FIND_REQUIRED)
+ENDIF (OpenSplice_FOUND)
+
+MARK_AS_ADVANCED(OpenSplice_INCLUDE_DIRS OpenSplice_LIBRARIES OpenSplice_IDLGEN_BINARY)
+#INCLUDE (MacroOpenSplice)
