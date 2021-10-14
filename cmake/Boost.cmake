88df16890ae6738dab32c03d241a0ae0dc27bec5
sivakumar101 <sivakumar.s.k.k@gmail.com>
2021-10-14 12:11:10 +1030

    remove program options from the boost installation

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -1,4 +1,3 @@
-cmake_minimum_required(VERSION 2.8)
-find_package(Boost 1.71 COMPONENTS program_options REQUIRED)
+find_package(Boost 1.71 COMPONENTS REQUIRED)
 include_directories(${Boost_INCLUDE_DIR})
 link_directories(${Boost_LIBRARY_DIR})
