5b2b7ce240f1a0607de544bfd3b5521c2152b11d
sivakumar101 <sivakumar.s.k.k@gmail.com>
2021-10-20 10:51:54 +1030

    Update the boost version to get it working on the docker

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -1,5 +1,5 @@
 if (UNIX)
-find_package(Boost 1.71 COMPONENTS REQUIRED)
+find_package(Boost 1.66 COMPONENTS REQUIRED)
 include_directories(${Boost_INCLUDE_DIR})
 link_directories(${Boost_LIBRARY_DIR})
 elseif (WIN32)
